eb5eb0dc2f6e6310c50b7438e44d283c9cf051ef
Ilya <55401238+Kolyamba2007@users.noreply.github.com>
2023-09-21 18:08:24 +0300

    Lib

--- Point.cpp
+++ Point.cpp
@@ -1,20 +1,8 @@
 #include "Point.h"
+#include "math.h"
 
-class Point
+float Point::Distance(Point point1, Point point2)
 {
-public:
-	int x, y, z;
-
-	Point(int x, int y, int z)
-	{
-		this->x = x;
-		this->y = y;
-		this->z = z;
-	};
-
-	static float Distance(Point point1, Point point2)
-	{
-		return sqrt(pow(point2.x - point1.x, 2) + pow(point2.y - point1.y, 2) +
-			pow(point2.z - point1.z, 2));
-	}
-};
+	return sqrt(pow(point2.x - point1.x, 2) + pow(point2.y - point1.y, 2) +
+		pow(point2.z - point1.z, 2));
+}
